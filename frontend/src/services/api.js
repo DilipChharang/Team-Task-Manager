115f7063d20f1e3604e2e7567a46be0438180dc9
Dilip Chharang <chharangdilip386@gmail.com>
2026-05-05 14:16:56 +0530

    Initial commit: TeamFlow Full-Stack App with Admin and Member roles

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "team-task-manager-production-3518.up.railway.app",
+  baseURL: "https://team-task-manager-production-3518.up.railway.app/api",
 });
 
 // token automatically add karega
